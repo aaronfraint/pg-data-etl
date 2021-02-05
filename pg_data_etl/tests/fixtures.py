cca72a2a855f47b42b233bebba09a4912eeed074
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-02-05 17:20:30 -0500

    add canned zip shp

--- pg_data_etl/tests/fixtures.py
+++ pg_data_etl/tests/fixtures.py
@@ -3,6 +3,8 @@ from pg_data_etl import Database, configurations
 import pytest
 import os
 import shutil
+import zipfile
+import urllib.request
 
 # Define database connections for the tests
 c = configurations()
@@ -13,8 +15,15 @@ production_gis_db = Database("gis", **c["dvrpc_gis"])
 
 # Create a directory to hold the test files
 test_data_path = r"C:\Users\afraint\Documents\pg_data_etl_tests"
+if not os.path.exists(test_data_path):
+    os.makedirs(test_data_path)
 
+# This is a shapefile we're going to make vis pgsql2shp
 shp_path = os.path.join(test_data_path, "test_shapefile")
+
+# This is a shapefile we'll download from the web and import with shp2pgsql
+shp_zip_url = "https://github.com/azavea/geo-data/raw/master/Neighborhoods_Philadelphia/Neighborhoods_Philadelphia.zip"
+zipped_shp_file_name = os.path.join(test_data_path, "Neighborhoods_Philadelphia.zip")
 
 
 def take_a_break(seconds: float = 1.0):
@@ -24,15 +33,14 @@ def take_a_break(seconds: float = 1.0):
 @pytest.fixture(scope="session", autouse=True)
 def test_data(request):
     """
-    Create a test data directory during the setup.
-    Remove the folder (+ all contents) at the end of the test suite.
+    TEARDOWN:
+    ---------
+        1) Remove the folder (+ all contents) at the end of the test suite.
     """
 
-    # Setup
-    os.makedirs(test_data_path)
-
     # Teardown
-    def remove_test_dir():
+    def remove_test_dir_and_db():
         shutil.rmtree(test_data_path)
+        local_db.drop_db()
 
-    request.addfinalizer(remove_test_dir)
+    request.addfinalizer(remove_test_dir_and_db)
