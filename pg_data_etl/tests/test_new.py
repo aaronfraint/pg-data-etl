4a628c3006aedcd3258e10a14903cb81c8bce9cc
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-02-05 13:13:22 -0500

    rename

--- pg_data_etl/tests/test_new.py
+++ pg_data_etl/tests/test_new.py
@@ -1,4 +1,4 @@
-from pg_data_etl.tests.fixtures import local_db, network_db_that_exists, take_a_break
+from pg_data_etl.tests.fixtures import local_db, take_a_break
 
 
 def test_creation_of_database():
@@ -20,9 +20,3 @@ def test_dropping_of_database():
     take_a_break()
 
     assert local_db.exists() is False
-
-
-def test_that_existing_database_exists():
-    """ This database definitely exists already """
-
-    assert network_db_that_exists.exists() is True
