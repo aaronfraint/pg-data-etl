6f48397719c8615537b29dc94bdb5b146c7fb356
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-02-05 13:13:13 -0500

    add more dbs

--- pg_data_etl/tests/fixtures.py
+++ pg_data_etl/tests/fixtures.py
@@ -4,8 +4,10 @@ from pg_data_etl import Database, configurations
 c = configurations()
 
 local_db = Database("test_from_pytest_lastone", **c["localhost"])
+local_sw_db = Database("sidewalk_gap_analysis", **c["localhost"])
 network_db_that_exists = Database("GTFS", **c["daisy"])
+production_gis_db = Database("gis", **c["dvrpc_gis"])
 
 
-def take_a_break(seconds: float = 0.5):
+def take_a_break(seconds: float = 1.0):
     time.sleep(seconds)
