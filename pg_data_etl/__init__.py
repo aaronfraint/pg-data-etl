d79eb10bd91f819e8314e15e04de75b44d0e3705
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-04-28 10:31:14 -0400

    rename file. update import

--- pg_data_etl/__init__.py
+++ pg_data_etl/__init__.py
@@ -1,4 +1,4 @@
 from .configuration_manager import configurations as connections
 import pg_data_etl.database.helpers as helpers
 import pg_data_etl.database.actions as actions
-from .database.db import Database
+from .database.Database import Database
