156b0a4eae0bbab81a7eba950ff831ff627759fc
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-05-05 08:07:42 -0400

    don't read config file until necessary

--- pg_data_etl/settings/make_config_file.py
+++ pg_data_etl/settings/make_config_file.py
@@ -1,7 +1,33 @@
 from pathlib import Path
 from typing import Union
 
-from ..settings import STARTER_CONFIG_FILE, DB_CONFIG_FILEPATH
+from pathlib import Path
+
+DB_CONFIG_FILEPATH = Path.home() / ".pg-data-etl" / "database_connections.cfg"
+
+STARTER_CONFIG_FILE = """
+[DEFAULT]
+pw = this-is-a-placeholder-password
+port = 5432
+super_db = postgres
+super_un = postgres
+super_pw = this-is-another-placeholder-password
+
+[localhost]
+host = localhost
+un = postgres
+pw = your-password-here
+
+[digitalocean]
+un = your-username-here
+host = your-host-here.db.ondigitalocean.com
+pw = your-password-here
+port = 98765
+sslmode = require
+super_db = your_default_db
+super_un = your_super_admin
+super_pw = some_super_password12354
+"""
 
 
 def make_config_file(
