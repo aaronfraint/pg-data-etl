fb6f487d2113289a6586f96c057027d5a9e14045
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-05-05 11:49:09 -0400

    clean up class and command runner

--- pg_data_etl/helpers/commands.py
+++ pg_data_etl/helpers/commands.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import subprocess
 from pathlib import Path
+from rich import print
 
 
 def run_command_in_shell(command: str) -> str:
@@ -15,8 +16,11 @@ def run_command_in_shell(command: str) -> str:
 
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
-    output = process.communicate()
-    print(output[0])
+    output, err = process.communicate()
+    print(output.decode("utf-8"))
+
+    if err:
+        print(err.decode("utf-8"))
 
     process.kill()
 
@@ -28,51 +32,91 @@ class CommandPathManager:
     TODO: docstring
     """
 
-    def __init__(self, psql_bin: str | None = None, tippecanoe_bin: str | None = None):
-        self.bins = {"psql": psql_bin, "tippecanoe": tippecanoe_bin}
-
-    def _add_bin_path_if_exists(self, cmd, bin: str = "psql") -> Path:
+    def __init__(self, **kwargs):
         """
-        TODO: docstring
+        - Define a dictionary of `bin_paths` for `psql` and `tippecanoe` as `None`
+        - Assign any other bin paths provided at runtime
+        """
+        self.bin_paths = {"psql": None, "tippecanoe": None}
+
+        self.set_bin_path(**kwargs)
+
+    @property
+    def configured_commands(self) -> list:
+        """
+        - Return a list of all command IDs that have been set
+        """
+        return list(self.bin_paths.keys())
+
+    def _add_bin_path_if_exists(self, cmd: str, bin_id: str = "psql") -> str | None:
+        """
+        - Generate a full path for a `cmd` if the `bin_id` is configured.
+        - Otherwise just return the `cmd` without a path, under the assumption that
+        it is already on the user's system path
+
+        Args:
+            cmd (str): the command to be run from a terminal
+            bin_id (str): the bin ID value to inherit the path from
+
+        Returns:
+            the full path or shorthand command
         """
 
-        if bin in self.bins.keys():
-            if not self.bins[bin]:
-                return cmd
+        if bin_id in self.configured_commands:
+            if self.bin_paths[bin_id]:
+                return str(Path(self.bin_paths[bin_id]) / cmd)
             else:
-                return Path(self.bins[bin]) / cmd
+                return cmd
         else:
-
-            print(f"{bin=} is not an option. Use one of: {self.bins.keys()}")
+            print(f"{bin_id=} is not an option. Use one of: {self.configured_commands}")
             return None
 
     def set_bin_path(self, **kwargs) -> None:
         """
-        - assign bin path to command using `cmd=bin_path` syntax
+        - Assign bin path to command using `cmd=bin_path` syntax
         """
         for k, v in kwargs.items():
-            self.bins[k] = v
+            self.bin_paths[k] = v
 
     @property
     def psql(self):
+        """
+        - `psql` connects to a postgres db
+        """
         return self._add_bin_path_if_exists("psql")
 
     @property
     def pg_dump(self):
+        """
+        - `pg_dump` exports a table or full database
+        """
         return self._add_bin_path_if_exists("pg_dump")
 
     @property
     def shp2pgsql(self):
+        """
+        - `shp2pgsql` imports shapefiles into postgres
+        """
         return self._add_bin_path_if_exists("shp2pgsql")
 
     @property
     def pgsql2shp(self):
+        """
+        - `pgsql2shp` exports spatial postgres data to shapefile
+        """
         return self._add_bin_path_if_exists("pgsql2shp")
 
     @property
     def ogr2ogr(self):
+        """
+        - `ogr2ogr` converts spatial data between a variety of formats
+        """
         return self._add_bin_path_if_exists("ogr2ogr")
 
     @property
     def tippecanoe(self):
+        """
+        - `tippecanoe` converts `.geojson` data into vector tiles
+        - This is the only command that is not expected to exist in the `psql` bin
+        """
         return self._add_bin_path_if_exists("tippecanoe", "tippecanoe")
