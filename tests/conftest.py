57d1efd45ae523405411e20361d5bcdb9a5b04ac
Aaron Fraint <aaronfraint@icloud.com>
2021-08-13 11:43:18 -0400

    revise configuration to work outside firewall

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from pg_data_etl import Database, config
+from pg_data_etl import Database
 import pytest
 import shutil
 import zipfile
@@ -6,7 +6,7 @@ import urllib.request
 from pathlib import Path
 
 # This is a temp folder where we'll store inputs/ouputs from the testing suite
-TEST_DATA_PATH = Path(r"C:\Users\afraint\Documents\pg_data_etl_tests")
+TEST_DATA_PATH = Path("./test_data")
 
 
 # This is a shapefile we'll download from the web and import with shp2pgsql
@@ -15,18 +15,10 @@ LOCAL_ZIPPED_FILEPATH = TEST_DATA_PATH / "Neighborhoods_Philadelphia.zip"
 
 
 @pytest.fixture(scope="function")
-def test_folder():
-    if not TEST_DATA_PATH.exists():
-        TEST_DATA_PATH.mkdir()
-
-    yield TEST_DATA_PATH
-
-
-@pytest.fixture(scope="function")
 def local_db():
     """ Spin up a local db, use it in the test, then drop it """
 
-    db = Database.from_parameters("test_from_pytest_lastone", **config["localhost"])
+    db = Database.from_config("pytest", "localhost")
 
     db.admin("CREATE")
 
@@ -35,46 +27,59 @@ def local_db():
     db.admin("DROP")
 
 
-@pytest.fixture(scope="function")
-def production_gis_db():
-    yield Database.from_parameters("gis", **config["dvrpc_gis"])
+# @pytest.fixture(scope="function")
+# def localhost_postgres():
+#     yield Database.from_parameters("postgres", **config["localhost"])
 
 
-@pytest.fixture(scope="function")
-def network_db_that_exists():
-    yield Database.from_parameters("GTFS", **config["daisy"])
+# @pytest.fixture(scope="function")
+# def production_gis_db():
+#     yield Database.from_parameters("gis", **config["dvrpc_gis"])
 
 
-@pytest.fixture(scope="function")
-def local_sw_db():
-    yield Database.from_parameters("sidewalk_gap_analysis", **config["localhost"])
+# @pytest.fixture(scope="function")
+# def network_db_that_exists():
+#     yield Database.from_parameters("GTFS", **config["daisy"])
+
+
+# @pytest.fixture(scope="function")
+# def local_sw_db():
+#     yield Database.from_parameters("sidewalk_gap_analysis", **config["localhost"])
 
 
 @pytest.fixture(scope="function")
 def downloaded_shapefile():
+
+    if not TEST_DATA_PATH.exists():
+        TEST_DATA_PATH.mkdir()
+
+    # Copy zipped shapefile to disk
     with urllib.request.urlopen(ZIPPED_SHAPEFILE_URL) as response, open(
         LOCAL_ZIPPED_FILEPATH, "wb"
     ) as out_file:
         shutil.copyfileobj(response, out_file)
 
+    # Extract the zip file
     with zipfile.ZipFile(LOCAL_ZIPPED_FILEPATH) as zf:
         zf.extractall(TEST_DATA_PATH)
 
-    unzipped_file_name = LOCAL_ZIPPED_FILEPATH.replace(".zip", "")
+    # provide the unzipped filepath
+    print(LOCAL_ZIPPED_FILEPATH)
+    unzipped_file_name = str(LOCAL_ZIPPED_FILEPATH).replace(".zip", "")
 
     yield unzipped_file_name
 
 
-@pytest.fixture(scope="session", autouse=True)
-def teardown_test_data_dir(request):
-    """
-    TEARDOWN:
-    ---------
-        1) Remove the folder (+ all contents) at the end of the test suite.
-    """
+# @pytest.fixture(scope="session", autouse=True)
+# def teardown_test_data_dir(request):
+#     """
+#     TEARDOWN:
+#     ---------
+#         1) Remove the folder (+ all contents) at the end of the test suite.
+#     """
 
-    # Teardown
-    def remove_test_dir_and_db():
-        shutil.rmtree(TEST_DATA_PATH)
+#     # Teardown
+#     def remove_test_dir_and_db():
+#         shutil.rmtree(TEST_DATA_PATH)
 
-    request.addfinalizer(remove_test_dir_and_db)
+#     request.addfinalizer(remove_test_dir_and_db)
