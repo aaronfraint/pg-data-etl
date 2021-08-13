a789d3070166d4ea4f7148ff5f3e0a5d01134b13
Aaron Fraint <aaronfraint@icloud.com>
2021-08-13 11:43:37 -0400

    update test

--- tests/test_shp2pgsql.py
+++ tests/test_shp2pgsql.py
@@ -1,10 +1,18 @@
-def test_shp2pgsql_imports_spatial_data(local_db, downloaded_shapefile):
+from pg_data_etl import Database
+
+
+def test_shp2pgsql_imports_spatial_data(local_db: Database, downloaded_shapefile):
     """
     Confirm that we have a spatial table in the DB after importing a local shapefile
     """
 
     unzipped_shp_file_name = downloaded_shapefile
 
-    local_db.shp2pgsql(unzipped_shp_file_name, 2272, "test.neighborhoods")
+    local_db.import_gis(
+        method="shp2pgsql",
+        srid=2272,
+        filepath=unzipped_shp_file_name,
+        sql_tablename="test.neighborhoods",
+    )
 
-    assert "test.neighborhoods" in local_db.spatial_table_list()
+    assert "test.neighborhoods" in local_db.tables(spatial_only=True)
